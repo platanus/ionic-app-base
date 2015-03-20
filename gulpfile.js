6c50b452451416057a60eb1885e68ebf378d67a3
René A. Morales <rene.morales.sanchez@gmail.com>
2015-03-20 10:16:44 -0300

    Initial Platanus commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,64 @@
+var fs = require('fs');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var bower = require('bower');
+var ignore = require('gulp-ignore');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
+var del = require('del');
+var ngConstant = require('gulp-ng-constant');
 var rename = require('gulp-rename');
-var sh = require('shelljs');
+var autoprefixer = require('gulp-autoprefixer');
+
+var env = gutil.env.env || 'development';
+var envPath = './environments/' + env + '.json'
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  sass: ['./app/scss/**/*.scss']
 };
 
 gulp.task('default', ['sass']);
+gulp.task('build', ['env', 'sass', 'cleanup-www', 'copy-ignore', 'autoprefix']);
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
+gulp.task('env', function () {
+  gulp.src(envPath)
+    .pipe(ngConstant({
+      name: 'app.env'
     }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+    .pipe(rename('env.js'))
+    .pipe(gulp.dest('./app/js'));
+});
+
+gulp.task('sass', function() {
+  gulp.src('./app/scss/app.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./app/css/'));
+});
+
+gulp.task('cleanup-www', function(){
+  del.sync(['./www']);
+});
+
+gulp.task('copy-ignore', function(){
+  var buildignore = fs.readFileSync('.buildignore', { encoding: 'utf-8' });
+  var lines = buildignore.split('\n');
+  var paths = [];
+
+  lines.forEach(function(line){
+    line = line.trim();
+    if ( line.indexOf('#') == 0 || line.length == 0 ) return;
+    if ( line[line.length-1] == '/' ) line = line + '**/*';
+    paths.push('!./app/'+line);
+  });
+
+  paths.unshift('./app/**/*');
+
+  gulp.src(paths)
+    .pipe(gulp.dest('./www/'));
+});
+
+gulp.task('autoprefix', function(){
+
 });
 
 gulp.task('watch', function() {
